9afd7aba388e635fee43837d1b0b72b56c6cc11f
nzwulfin <nzwulfin@gmail.com>
2013-10-18 16:51:44 -0400

    Revert "handle blank key"
    This reverts commit 3a86bdc62b4920eddccc7c9d90a1edbdc47adbab.

--- billing.py
+++ billing.py
@@ -17,16 +17,13 @@ reader = csv.DictReader(f)
 
 # Loop through the resulting dictionary, find everything associated with a specific
 # key located in a column and dump to those records to separate files.
-keys = []
 
 # Since we are using python 2.6 we can't use the writeheader method so this is mainly
 # a hacky loop to work around that.  There's probably a better way than opening and
 # closing the file in a different mode for every write.
 for row in reader:
-	acct = row[col] or 'totals'      
+	acct = row[col]
 	filename = acct + '-' + ts + '-billing.csv'
-	if row[col] not in keys:
-		keys.append(row[col])
 
 	if os.path.exists(filename):
 		out = open(filename, 'a')
@@ -36,9 +33,10 @@ for row in reader:
 
 	else:
 		out = open(filename, 'w')
+		print "Creating " + filename + " now"
 		header = csv.writer(out)
 		header.writerow(reader.fieldnames)
 		out.close()
 
+
 f.close()
-print ', '.join(keys)
